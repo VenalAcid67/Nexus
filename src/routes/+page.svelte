93c7291be6ec0a6a59a6541ecbff8b3df05019a8
VenalAcid67 <224954791+VenalAcid67@users.noreply.github.com>
2026-08-15 18:07:18 +0530

    Configured Tauri Shell and A Placeholder UI

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<main class="container">
+  <h1>Nexus</h1>
+</main>
+
+<style>
+  .container {
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100vh;
+  }
+</style>
